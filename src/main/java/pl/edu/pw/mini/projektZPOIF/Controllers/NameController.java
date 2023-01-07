27bf86c2a4c94adc6e1800012da2c543837b7374
Xevi8X <wggajda@gmail.com>
2023-01-07 22:47:07 +0100

    Quick fix

--- src/main/java/pl/edu/pw/mini/projektZPOIF/Controllers/NameController.java
+++ src/main/java/pl/edu/pw/mini/projektZPOIF/Controllers/NameController.java
@@ -3,14 +3,12 @@ package pl.edu.pw.mini.projektZPOIF.Controllers;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.edu.pw.mini.projektZPOIF.Services.TcpService;
 
 @Tag(name = "Name")
 @RestController
+@RequestMapping("/bulbs")
 public class NameController {
     final TcpService tcpService;
 
